47a7f38d92609d4f584e45b1c295c14ad6d881e8
Artem <artm-porjad@mail.ru>
2022-02-15 03:46:55 +0300

    example edited

--- backend/example.py
+++ backend/example.py
@@ -1,6 +1,9 @@
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
+from starlette.authentication import AuthenticationBackend, AuthCredentials, SimpleUser
 from starlette.routing import Route
+from starlette.middleware import Middleware
+from starlette.applications import Starlette
+from starlette.middleware.authentication import AuthenticationMiddleware
+from starlette.responses import JSONResponse
 import notifier.client
 
 
@@ -14,7 +17,30 @@ async def homepage(request):
     return JSONResponse({'hello': 'world'})
 
 
-app = Starlette(debug=True, routes=[
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, request):
+        if not request.cookies.get("auth"):
+            return
+        login = request.cookies.get("auth")
+        return AuthCredentials(["authenticated"]), SimpleUser(login)
+
+
+routes = [
     Route('/', homepage),
     Route('/registration-complete', send_by_email_after_registration)
-])
+]
+
+
+
+
+
+middleware = [
+    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+]
+
+app = Starlette(routes=routes, middleware=middleware)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
